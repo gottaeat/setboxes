af15a25e37904bc320c19a619ff9c566256303f8
must_eat <mss@tutanota.de>
2024-05-01 04:10:22 +0300

    rwr: (provision/) mkimg.sh; split off to functions, make every invocation except for partclone quiet.

--- provision/mkimg.sh
+++ provision/mkimg.sh
@@ -1,33 +1,41 @@
 #!/bin/bash
-. ./common
-
 set -e
 
-if [ "$(id -u)" -ne 0 ]; then
-    perr "need root big man"
-fi
+main(){
+    . ./common
 
-_TSTAMP="$(date '+%Y%m%d_%H%M%S')"
-_IMGDIR="/mnt/mss/stuff/techy-bits/images"
+    rootcheck
+    nbdcheck
 
-if [ ! -d "${_IMGDIR}" ]; then mkdir -pv "${_IMGDIR}"; fi
+    _TSTAMP="$(date '+%Y%m%d_%H%M%S')"
+    _IMGDIR="/mnt/mss/stuff/techy-bits/images"
 
-for volume in ./xml/vol_*.xml; do
-    volname="$(echo "${volume}" | sed 's/\.\/xml\/vol_//g;s/\.xml$//g')"
-    path_to_vol="$(virsh vol-key --pool setboxes --vol ${volname}.qcow2)"
+    if [ ! -d "${_IMGDIR}" ]; then mkdir -pv "${_IMGDIR}"; fi
 
-    virsh destroy --graceful --domain "${volname}" || true
-    qemu-nbd --connect=/dev/nbd1 "${path_to_vol}"
+    iterate_xmls
+}
 
-    e2fsck -fy /dev/nbd1p2 || true
+iterate_xmls(){
+    for volume in ./xml/vol_*.xml; do
+        volname="$(echo "${volume}" | sed 's/\.\/xml\/vol_//g;s/\.xml$//g')"
+        path_to_vol="$(virsh vol-key --pool setboxes --vol ${volname}.qcow2)"
 
-    partclone.fat32 -q -c -s /dev/nbd1p1 -o - \
-        | zstd -T`nproc` -19 -o \
-            "${_IMGDIR}"/"${_TSTAMP}"-setboxes-"${volname}"-p1.img.zst
+        pinfo "processing ${volname}"
+        pinfo "(${path_to_vol})"
 
-    partclone.ext4 -q -c -s /dev/nbd1p2 -o - \
-        | zstd -T`nproc` -19 -o \
-            "${_IMGDIR}"/"${_TSTAMP}"-setboxes-"${volname}"-p2.img.zst
+        virsh destroy --graceful --domain "${volname}" >/dev/null 2>&1 || true
+        qemu-nbd --connect=/dev/nbd1 "${path_to_vol}" >/dev/null 2>&1
 
-    qemu-nbd --disconnect /dev/nbd1
-done
+        e2fsck -fy /dev/nbd1p2 >/dev/null 2>&1 || true
+
+        partclone.fat32 -q -c -s /dev/nbd1p1 -o - \
+            | zstd -T`nproc` -19 -o \
+                "${_IMGDIR}"/"${_TSTAMP}"-setboxes-"${volname}"-p1.img.zst
+
+        partclone.ext4 -q -c -s /dev/nbd1p2 -o - \
+            | zstd -T`nproc` -19 -o \
+                "${_IMGDIR}"/"${_TSTAMP}"-setboxes-"${volname}"-p2.img.zst
+
+        qemu-nbd --disconnect /dev/nbd1 >/dev/null 2>&1
+    done
+}
